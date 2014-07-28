e6902223e52549cf4659274349dee1637fbe9116
m1ke <git@m1ke.me>
2014-07-28 12:14:36 +0100

    JS min

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var gulp = require('gulp');
 var preprocess=require('gulp-preprocess')
 	,replace=require('gulp-replace')
 	,sass=require('gulp-ruby-sass')
+	,jsmin=require('gulp-jsmin')
 	;
 
 gulp.task('scripts', function() {
   gulp.src(['./jspp/*.js'])
     .pipe(preprocess())
+    .pipe(jsmin())
     .pipe(gulp.dest('./js/'))
 });
 
--- js/home.js
+++ js/home.js
@@ -1,28 +1,3 @@
 
-
-$(function(){
-	$('.stripe-pay').click(function(e){
-		e.preventDefault();
-		var value=$('[name="pay"]').val();
-		if (!value){
-			alert('You must enter a value to send a payment.');
-			return false;
-		}
-		var handler=StripeCheckout.configure({
-			key:$(this).data('stripe'),
-			image:'http://gcskydiving.com/images/avatar-128.png',
-			token:function(token){
-				var $input=$('[name="stripe"]').val(token.id);
-				$input.closest('form').submit();
-			}
-		});
-		handler.open({
-			name: 'Ground Control Skydiving'
-			,description: 'Subscription ($'+value+')'
-			,amount: (value*100)
-			,email: 'info@gcskydiving.com'
-			,currency: $('[name="currency"]').val()
-			,panelLabel: 'Pay for Ground Control!'
-		});
-	}).removeClass('btn-warning').addClass('btn-primary').text('Pay now');
-});
+$(function(){$('.stripe-pay').click(function(e){e.preventDefault();var value=$('[name="pay"]').val();if(!value){alert('You must enter a value to send a payment.');return false;}
+var handler=StripeCheckout.configure({key:$(this).data('stripe'),image:'http://gcskydiving.com/images/avatar-128.png',token:function(token){var $input=$('[name="stripe"]').val(token.id);$input.closest('form').submit();}});handler.open({name:'Ground Control Skydiving',description:'Subscription ($'+value+')',amount:(value*100),email:'info@gcskydiving.com',currency:$('[name="currency"]').val(),panelLabel:'Pay for Ground Control!'});}).removeClass('btn-warning').addClass('btn-primary').text('Pay now');});
